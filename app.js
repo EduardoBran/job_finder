42f089e03b28a3c832929ed7cd80c5b9248861b4
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-03-16 22:53:41 -0300

    conectando ao banco de dados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
@@ -7,6 +8,18 @@ app.listen(PORT, function() {
     console.log(`O express está rodando na porta ${PORT}`);
 });
 
+//db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar", err);
+    });
+
+
+//routes
 app.get('/', (req, res) => {
     res.send("Está funcionando!");
 });
