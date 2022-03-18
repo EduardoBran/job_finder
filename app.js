35392ff80422e18e3cb91d199e511c86b69565ac
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-03-18 15:09:24 -0300

    instalando o handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
-const db = require('./db/connection');
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
 const bodyParser = require('body-parser');
 
 const PORT = 3000;
@@ -12,6 +14,10 @@ app.listen(PORT, function() {
 //body parser
 app.use(bodyParser.urlencoded({extended: false}));
 
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //db connection
 db
